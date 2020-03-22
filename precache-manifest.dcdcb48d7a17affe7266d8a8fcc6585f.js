55f7347ad79904cac539ed5dd80687ac921783d8
Artyom <artyom.zayarny@gmail.com>
2020-03-22 20:06:20 +0200

    Updates

---
+++ precache-manifest.dcdcb48d7a17affe7266d8a8fcc6585f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d50a65acb90bb0dbe828381a72f38e",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "6850cb6c34fd492e9cb9",
+    "url": "/react-pagination/static/css/main.7920ebb6.chunk.css"
+  },
+  {
+    "revision": "c78abe704206e23375b7",
+    "url": "/react-pagination/static/js/2.f7c03568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.f7c03568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6850cb6c34fd492e9cb9",
+    "url": "/react-pagination/static/js/main.0f249763.chunk.js"
+  },
+  {
+    "revision": "34716dd90a10eac4641d",
+    "url": "/react-pagination/static/js/runtime-main.4a253d51.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-pagination/static/media/logo.ee7cd8ed.svg"
+  }
+]);
